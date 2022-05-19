41fa00059b395345db990b538de1ae866e6e9800
1ning <s221182838@deakin.edu.au>
2022-05-19 16:00:56 +1000

    Initial commit

---
+++ app/src/main/java/com/example/task61d/Global.java
@@ -0,0 +1,9 @@
+package com.example.task61d;
+
+public class Global {
+    public static int id;
+    public static String sender;
+    public static String orderid;
+    public static int time;
+    public static double money;
+}
